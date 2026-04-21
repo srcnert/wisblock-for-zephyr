645b2bd3c8998c68ea12c5a2cf33d28c5269ecda
srcnert <sercanerat@gmail.com>
2026-04-21 10:16:28 +0300

    Updated make_wav.py file to add gain support.

--- app/sensor/rak18030/script/make_wav.py
+++ app/sensor/rak18030/script/make_wav.py
@@ -6,11 +6,24 @@ raw_data_block = """
 <AUDIO DATA>
 """
 
-samples = [int(x.strip()) for x in raw_data_block.replace('\n', '').split(',') if x.strip()]
-
 sample_rate = 16000
 
-with wave.open('app/basic/microphone/script/mic_test.wav', 'w') as f:
+with wave.open('mic_test.wav', 'w') as f:
+    gain = 4 # If you are trying to determine what gain to add to the microphone, you can
+             # temporarily increase this above 1 (or below) to find the correct gain
+             # this number must be 1 for actual model creation as the audio won't
+             # be the same on device if this is not 1!
+             # The percentage of clipped samples should be kept low (<1%)
+             # If you aren't getting the number of samples you expect or much lower (eg 2 when you should have 8)
+             # try increasing the gain here, and when you find the correct number update it on the device
+
+    samples = [int(gain*int(x.strip())) for x in raw_data_block.replace('\n', '').split(',') if x.strip()]
+
+    print("Verification of Gain settings:")
+    print("    Maximum found:", max(samples), "  32767 is maximum allowable")
+    print("    Minimum found:", min(samples), " -32767 is minimum allowed")
+
+    # Set the parameters for the WAV file
     f.setnchannels(1)       # Mono
     f.setsampwidth(2)       # 2 bytes per sample (16-bit)
     f.setframerate(sample_rate)
